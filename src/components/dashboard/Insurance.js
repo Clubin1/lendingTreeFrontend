bb316afbfdff780e19ce2279b5d665e98ce0cb9c
Justin Tubay <justin.tubay@gmail.com>
2021-07-30 01:24:03 -0400

    mobile queries

--- src/components/dashboard/Insurance.js
+++ src/components/dashboard/Insurance.js
@@ -27,19 +27,21 @@ class Insurance extends React.Component {
         return (
           <div className="serviceWrapper2">
           <div className="serviceCenter2">
-            <h4            
+      <div id="twooo">
+      <h4            
             data-aos-anchor-easing='ease-in-out'
             data-aos='fade-up'
             data-aos-offset='200' 
-            className="serviceTitle twotime">
+            className="serviceTitle asdasdasdasdsad twotime">
                 Compare insurance qoutes for free
             </h4>
             <h6 
                         data-aos-anchor-easing='ease-in-out'
                         data-aos='fade-up'
                         data-aos-offset='200'
-            className="serviceText twotime">Shop and compare qoutes to get the best coverage 
+            className="serviceText twotime sdfsdfsdfsdf">Shop and compare qoutes to get the best coverage 
             for the best rate.</h6>
+      </div>
 
            <Link>
            <div
@@ -48,7 +50,7 @@ class Insurance extends React.Component {
             data-aos-offset='200'
             className="serviceItem">
                 <h5 className="serviceItemColor1"> Auto Insurance </h5>
-                <h6 className="serviceText">Find insurance to protect your vehicle.</h6>
+                <h6 className="serviceText ggg">Find insurance to protect your vehicle.</h6>
                 <div className="serviceItemBtn1">
                 Compare rates
               </div>
@@ -62,7 +64,7 @@ class Insurance extends React.Component {
                         data-aos-delay='50'
             className="serviceItem">
               <h5 className="serviceItemColor2"> Home Insurance </h5>
-              <h6 className="serviceText">Get insurance to provide coverage for unseen events.</h6>
+              <h6 className="serviceText ggg">Get insurance to provide coverage for unseen events.</h6>
               <div className="serviceItemBtn3">
                Get a free qoute
               </div>
@@ -77,7 +79,7 @@ class Insurance extends React.Component {
                                     data-aos-delay='100'
             className="serviceItem">
               <h5 className="serviceItemColor3">Life Insurance </h5>
-              <h6 className="serviceText">Protect your family with affordable life insurance from top online providers. We help you get the right policy at the right price.</h6>
+              <h6 className="serviceText ggg">Protect your family with affordable life insurance from top online providers. We help you get the right policy at the right price.</h6>
               <div className="serviceItemBtn2">
                 Stay protected
               </div>
